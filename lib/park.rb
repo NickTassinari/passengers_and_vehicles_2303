6fb3c788ed9aafa9fc51a83380d5f7498f081e8c
Nick Tassinari <ntass7800@gmail.com>
2023-04-05 09:54:39 -0600

    finish iteration 3

---
+++ lib/park.rb
@@ -0,0 +1,26 @@
+class Park 
+  attr_reader :name, :admission_price, :vehicles, :passengers
+
+  def initialize(name, admission_price)
+    @name = name
+    @admission_price = admission_price
+    @vehicles = []
+  end
+
+  def add_vehicle(vehicle)
+    @vehicles << vehicle
+  end
+
+  def attendees
+    @vehicles.flat_map {|vehicle| vehicle.passengers}
+  end
+
+
+  def calculate_revenue
+    revenue = 0 
+    @vehicles.map do |vehicle|
+      revenue += vehicle.num_adults * admission_price
+    end
+    revenue 
+  end
+end
