59c0bc574205b1aebf4284bc7700dc2978ceb3fc
Nick Tassinari <ntass7800@gmail.com>
2023-04-05 09:12:41 -0600

    finish iteration 1

---
+++ lib/passenger.rb
@@ -0,0 +1,26 @@
+class Passenger 
+  attr_reader :name, :age, :driver  
+
+  def initialize(attributes)
+    @name = attributes.fetch("name")
+    @age = attributes.fetch("age")
+    @driver = false 
+  end
+
+  def adult? 
+    if @age >= 18
+      true 
+    else 
+      false 
+    end
+  end
+
+  def driver? 
+    @driver 
+  end
+
+  def drive 
+    @driver = true 
+  end
+
+end
